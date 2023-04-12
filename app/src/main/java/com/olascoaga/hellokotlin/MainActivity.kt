c87293bb214f270ef68877e9bf7304ad91c36575
Uriel <uriel1396@gmail.com>
2023-04-11 20:55:38 -0600

    Added Text To Speech in hello kotlin

--- app/src/main/java/com/olascoaga/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/olascoaga/hellokotlin/MainActivity.kt
@@ -2,20 +2,40 @@ package com.olascoaga.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    var tts: TextToSpeech? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupUI()
+        tts = TextToSpeech(this, this)
+        setupListeners()
     }
 
-    fun setupUI() {
+    private fun setupListeners() {
+        findViewById<Button>(R.id.btnPlay).setOnClickListener {
+            speak()
+        }
+    }
+
+    private fun speak() {
         var message: String = findViewById<TextView>(R.id.tvHello).text.toString()
 
-        Log.i("Message tvHello", message)
+        tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            findViewById<TextView>(R.id.tvHello).text = "Hello Kotlin!"
+            tts!!.setLanguage(Locale.US)
+        } else {
+            findViewById<TextView>(R.id.tvHello).text = "Not available :c"
+        }
     }
 }
