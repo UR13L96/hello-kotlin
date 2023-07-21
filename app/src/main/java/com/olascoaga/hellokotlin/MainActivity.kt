198d787ae73ae35c8b37490ace9359463c4a66fc
Uriel <uriel1396@gmail.com>
2023-07-21 09:53:23 -0600

    Added spanish language and following best practices

--- app/src/main/java/com/olascoaga/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/olascoaga/hellokotlin/MainActivity.kt
@@ -3,13 +3,12 @@ package com.olascoaga.hellokotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
-    var tts: TextToSpeech? = null
+    private var tts: TextToSpeech? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,17 +24,30 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     }
 
     private fun speak() {
-        var message: String = findViewById<TextView>(R.id.tvHello).text.toString()
+        var message: String = findViewById<TextView>(R.id.etMessage).text.toString()
+
+        if (message.isEmpty()) {
+            findViewById<TextView>(R.id.tvHello).text = getString(R.string.tts_message_empty)
+            message = "¿Me quieres ver la cara de estúpida?"
+        }
 
         tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
-            findViewById<TextView>(R.id.tvHello).text = "Hello Kotlin!"
-            tts!!.setLanguage(Locale.US)
+            findViewById<TextView>(R.id.tvHello).text = getString(R.string.tts_ready)
+            tts!!.language = Locale("es_MX")
         } else {
-            findViewById<TextView>(R.id.tvHello).text = "Not available :c"
+            findViewById<TextView>(R.id.tvHello).text = getString(R.string.tts_not_available)
         }
+    }
+
+    override fun onDestroy() {
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
     }
 }
